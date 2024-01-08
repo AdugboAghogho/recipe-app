ba4fc19e0df82069e255672eebb160a7ddb2a8d5
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-08 10:47:46 +0100

    starting the project

---
+++ src/Components/Veggie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Veggie = () => {
+  return (
+    <div>
+      Veggie
+    </div>
+  )
+}
+
+export default Veggie
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Veggie from '../Components/Veggie'
+import Popular from '../Components/Popular'
+
+const Home = () => {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  )
+}
+
+export default Home
