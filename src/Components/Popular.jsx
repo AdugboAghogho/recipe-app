0fcd8488b42002d6a48be93d9a06391e616d312d
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-08 12:14:54 +0100

    added img

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { Spilde, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css';
 
 const Popular = () => {
+    const [popular, setPopular] = useState([]);
 
    useEffect(() => {
     getPopular();
@@ -11,14 +15,43 @@ const Popular = () => {
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=19`
       );
       const data = await api.json();
-      console.log(data);
+      setPopular(data.recipes)
+      console.log(data.recipes)
    };
 
   return (
     <div>
-      Popular
+      {popular.map((recipe) => {
+        return (
+            <Wrapper>
+                <h3>Popular Recipes</h3>
+                {popular.map((recipe) => {
+                    return (
+                        <Card>
+                            <p>{recipe.title}</p>
+                            <img src={recipe.image} alt={recipe.title} />
+                        </Card>
+                    )
+                })}
+            </Wrapper>
+        );
+      })}
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img{
+    border-radius: 2rem 10rem;
+  }
+`;
 
 export default Popular
