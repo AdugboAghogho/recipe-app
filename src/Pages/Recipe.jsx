2543d1c268670fc70eaf0d0997993be290f637b7
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-09 14:24:59 +0100

    fixing the button

--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -6,23 +6,32 @@ function Recipe() {
     let params = useParams();
     const [details, setDetails] = useState({});
 
-    const  fetchDetails = async () => {
+    useEffect(() => {
+        fetchDetails();
+    }, [params.name]);
+
+    const fetchDetails = async () => {
         const data = await fetch(
             `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
         );
         const detailData = await data.json(); 
-        setDetails(detailData);   
+        setDetails(detailData);
 
-        useEffect(() => {
-            fetchDetails();
-        }, [params.name]);
     };
 
   return (
-    <div>
-      Recipe
-    </div>
-  )
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+
+      <Info>
+        <Button>Instructions</Button>
+        <Button>Ingredients</Button>
+      </Info>
+    </DetailWrapper>
+  );
 }
 
 const DetailWrapper = styled.div`
@@ -31,33 +40,40 @@ const DetailWrapper = styled.div`
     display: flex;
 
     .active{
-    background: linear-gradient(35deg, #494949, #313131);
-    color: white;
+        background: linear-gradient(35deg, #494949, #313131);
+        color: white;
     }
 
     h2{
-    margin-bottom: 2rem;
+        margin-bottom: 2rem;
     }
+
     li{
-    font-size: 1.2rem;
-    line-height: 2.5rem;
+        font-size: 1.2rem;
+        line-height: 2.5rem;
     }
+
     ul{
-    margin-top: 2rem;
+        margin-top: 2rem;
+    }
+
+    img{
+        border-radius: 2rem;
     }
 `;
 
-const Button =styled.button `
+const Button = styled.button `
     padding: 1rem 2rem;
     color: #313131;
     background: white;
     border: 2px solid black;
     margin-right: 2rem;
     font-weight: 600;
+    border-radius: 5rem 2rem;
 `;
 
-const Info =styled.div`
-
+const Info = styled.div`
+    margin-left: 10rem;
 `;
 
 export default Recipe
