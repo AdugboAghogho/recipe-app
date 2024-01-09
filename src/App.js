1dc398db5e55490530ca8e9fd47a8535bf61d0d5
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-09 12:12:26 +0100

    done with the searched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./Pages/pages";
 import Category from "./Components/Category";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./Components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
